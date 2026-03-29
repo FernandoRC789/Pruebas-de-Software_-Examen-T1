6cc886b2adadc19f36fb3cb67f3fe4f4340054fc
FernandoRC789 <nickfernando789@gmail.com>
2026-03-29 16:40:53 -0500

    Parte 2 - test 2
    Parte 2 - Test 2 _ Prueba de test 2 Productos sin stock

--- EX_T1_Parte2_NickRodriguez/src/test/java/nickParte2/OrdenServiceTest.java
+++ EX_T1_Parte2_NickRodriguez/src/test/java/nickParte2/OrdenServiceTest.java
@@ -1,4 +1,44 @@
+package nickParte2;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 class OrdenServiceTest {
-  
+
+    @Test
+    void clienteNoExiste() {
+
+        // MOCKS
+        ClienteService clienteMock = mock(ClienteService.class);
+        ProductoService productoMock = mock(ProductoService.class);
+
+        // COMPORTAMIENTO
+        when(clienteMock.existeCliente("C1")).thenReturn(false);
+
+        OrdenService ordenService = new OrdenService(clienteMock, productoMock);
+
+        String r = ordenService.registrarOrden("C1", "P1", 1, 100);
+
+        assertEquals("Cliente no existe", r);
+    }
+
+    @Test
+    void productoSinStock() {
+
+        ClienteService clienteMock = mock(ClienteService.class);
+        ProductoService productoMock = mock(ProductoService.class);
+
+        when(clienteMock.existeCliente("C1")).thenReturn(true);
+        when(clienteMock.clienteActivo("C1")).thenReturn(true);
+        when(productoMock.tieneStock("P1", 5)).thenReturn(false);
+
+        OrdenService ordenService = new OrdenService(clienteMock, productoMock);
+
+        String r = ordenService.registrarOrden("C1", "P1", 5, 200);
+
+        assertEquals("Sin stock", r);
+    }
 }
